ef3d944f23dc7136258f3c34efcd276d721d3998
FocusKing <104179717+FocusKing@users.noreply.github.com>
2022-06-27 23:17:55 -0400

    input notes

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Note Taker Starter Code
+# Keep Track Note Taker
+
+## Resources 
+[GitHubLink](https://github.com/FocusKing/Keep-Track-Note-Taker)
+
+
+[Screencastify-Link]()
+## Badges
+![stayCLEAN](https://img.shields.io/badge/stay-CLEAN-blue)
+
+## Description
+GIVEN a note-taking application
+WHEN I open the Note Taker
+THEN I am presented with a landing page with a link to a notes page
+WHEN I click on the link to the notes page
+THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+WHEN I enter a new note title and the note’s text
+THEN a Save icon appears in the navigation at the top of the page
+WHEN I click on the Save icon
+THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+WHEN I click on an existing note in the list in the left-hand column
+THEN that note appears in the right-hand column
+WHEN I click on the Write icon in the navigation at the top of the page
+THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+## Table of Contents 
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Visuals](#visuals)
+- [Credits](#credits)
+- [License](#license)
+- [Test](#tests)
+
+## Installation
+Our challenge is to create an application called Note Taker that can be used to write and save notes. This application will use an Express.js back end and will save and retrieve note data from a JSON file.
+
+## Usage
+AS A manager
+I WANT to generate a webpage that displays my team's basic info
+SO THAT I have quick access to their emails and GitHub profiles  
+
+## Visuals
+![Keep Track image](./images/Dream%20Team%20Generator.png)
+## Credits
+ I was fortunate enough to get additional assistance from class mates either during office hours or during private study group sessions after class. I truly appreciate any help that I have received throughout this project.
+
+## License
+MIT License
+
+Copyright (c) [2022] [Marquise Allen]
+
+## Tests
+Great way to test the program is by using node index.js, review the terminal for any errors. You can also console log data to see how the program is running and what information is being pulled through. 
--- server.js
+++ server.js
@@ -1,48 +1,83 @@
-const espress =require('express');
-const path =require('path');
-const fs = require('fs');
-const notes = require('./Develop/db');
-const uuid = require('uuid');
-
-const PORT = process.env.PORT || 3001;
-
+const express = require('express');
+const { Http2ServerRequest } = require('http2');
 const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3001;   // connect to your port 3001
+const fs = require('fs');
+const notes = require('./Develop/db/db.json');
+const uuid = require('./Develop/utils/utils');
 
 app.use(express.json());
-app.use(expres.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true}));
 
 app.use(express.static('public'));
-// followed along with instructor
-app.get('/notes',(req,res) => {
-    res.sendFile(path.join(_dirname,'public/index.html'));
-})
-
-app.get('/',(req,res) => {
-    res.sendFile(path.join(_dirname,'public/index.html'));
-})
-
 
 app.get('/api/notes',(req,res) => {
     res.json(notes);
 });
-// paths will not collide and can have the same route because the run on different methods
-app.post('/api/notes',(req,res) => {
-    res.json(req.body);
-// this push context from the body to notes
-
-const updateNote = { ...req.body, id: uuid() };
-    notes.push(req.body);
-
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-        err ? res.json(err) : res.send('success');
-    });
-});
 
 
-app.delete('/api/notes/:id', (req,res) => {
- const filteredNotes = notes.filter(note => note.id === id);
+app.get('/notes',(req,res) => {
+    res.sendFile(path.join(__dirname,'./Develop/public/notes.html'));
+})
 
-});
+
+app.get('/',(req,res) => {
+    res.sendFile(path.join(__dirname,'./Develop/public/index.html'));
+})
+
+
+app.get('*',(req,res) => {
+    res.sendFile(path.join(__dirname,'./Develop/public/index.html'));
+})
+
+
+
+
+// The following HTML routes should be created:
+
+// GET /notes should return the notes.html file.
+
+// GET * should return the index.html file.
+
+// The following API routes should be created:
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// POST /api/notes should receive a new note to save on the request body, 
+// add it to the db.json file, and then return the new note to the client. 
+// You'll need to find a way to give each note a unique id when it's saved 
+// (look into npm packages that could do this for you).
+
+
+
+
+
+// // followed along with instructor info taking from activity 7 as well
+
+
+
+
+
+
+// // paths will not collide and can have the same route because the run on different methods
+// app.post('/api/notes',(req,res) => {
+//     res.json(req.body);
+// // this push context from the body to notes
+
+// const updateNote = { ...req.body, id: uuid() };
+//     notes.push(req.body);
+
+//     fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
+//         err ? res.json(err) : res.send('success');
+//     });
+// });
+
+
+// app.delete('/api/notes/:id', (req,res) => {
+//  const filteredNotes = notes.filter(note => note.id === id);
+
+// });
 
 // app.use(express.json());
 // app.use(expres.urlencoded({ extended: true}));
@@ -50,4 +85,6 @@ app.delete('/api/notes/:id', (req,res) => {
 // app.use(express.static('public'));
 // app.use(baseRoutes);
 
-app.listen(PORT);
+app.listen(PORT, function() {
+    console.log(`app is listening at http://localhost:${PORT}`);
+});
